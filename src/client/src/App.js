71d87cdb51ba418a788124d60365524ad1717d0a
Sujana Bhandari <sujana6589@gmail.com>
2022-08-23 13:23:47 +0200

    Bootstrap a react template project

---
+++ src/client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+
+    return (
+        <div>
+            <p>hello world</p>
+        </div>
+    )
+}
+
+export default App;
